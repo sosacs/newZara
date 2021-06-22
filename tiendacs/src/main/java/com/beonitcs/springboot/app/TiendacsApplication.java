7198c02018e3ee4f32a2bc0a329d28602f8b4a40
Cristian <scristiansosa@gmail.com>
2021-06-22 02:44:14 -0300

    api base

---
+++ tiendacs/src/main/java/com/beonitcs/springboot/app/TiendacsApplication.java
@@ -0,0 +1,13 @@
+package com.beonitcs.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendacsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendacsApplication.class, args);
+	}
+
+}
---
+++ tiendacs/src/test/java/com/beonitcs/springboot/app/TiendacsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beonitcs.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendacsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
